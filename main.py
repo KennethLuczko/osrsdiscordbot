29d2f2c26018e586f441976a9c710e117c14a90d
CataclysmGRIZZLY <hedmanpetterssondaniel@gmail.com>
2021-12-19 22:18:29 +0100

    ugly random

--- main.py
+++ main.py
@@ -11,13 +11,60 @@ bot = commands.Bot(command_prefix='!')
 
 # ----- THEATER OF BLOOD
 @bot.command(name='tob', help='Returns the loot from Theater of Blood.')
-async def tob(tob):
-    tob_loot = [
-        'Scythe of Vitur',
-        'Sanguinesti Staff',
-        "Justicar Helmet"
-    ]
-    response = random.choice(tob_loot)
+async def tob(tob, arg):
+    if isinstance(int(arg), int):
+        scythes = 0
+        rapiers = 0
+        staves = 0
+        faceguards = 0
+        chestguards = 0
+        legguards = 0
+        hilts = 0
+        cabbages = 0
+        for i in range(0, int(arg)+1):
+            diceroll = random.uniform(0, 173)
+            if diceroll <= 1:
+                scythes += 1
+            elif diceroll <= 2:
+                rapiers += 1
+            elif diceroll <= 4:
+                staves += 1
+            elif diceroll <= 6:
+                faceguards += 1
+            elif diceroll <= 8:
+                chestguards += 1
+            elif diceroll <= 10:
+                legguards += 1    
+            elif diceroll <= 21:
+                hilts += 1
+            else:
+                cabbages += 1
+        response = "Cabbages: " + str(cabbages) + "\n"
+        response += "Hilts: " + str(hilts) + "\n"
+        response += "Rapiers: " + str(rapiers) + "\n"
+        response += "Staves: " + str(staves) + "\n"
+        response += "Faceguards: " + str(faceguards) + "\n"
+        response += "Chestguards: " + str(chestguards) + "\n"
+        response += "Legguards: " + str(legguards) + "\n"
+        response += "Scythes: " + str(scythes) + "\n"
+
+    else:
+        diceroll = random.uniform(0, 173)
+        if diceroll <= 1:
+            response = "Scythe of Vitur (" + str(diceroll) + ")"
+        elif diceroll <= 10:
+            decent_loot = [
+                'Ghazri Rapier',
+                'Sanguinesti Staff',
+                'Justiciar Faceguard',
+                'Justiciar Chestguard',
+                'Justiciar Legguards'
+            ]
+            response = random.choice(decent_loot) + " (" + str(diceroll) + ")"
+        elif diceroll <= 21:
+            response = "Avernic defender hilt (" + str(diceroll) + ")"
+        else:
+            response = "Cabbage get fukt (" + str(diceroll) + ")"
     await tob.send(response)
 
 # ----- CHAMBERS OF XERIC
