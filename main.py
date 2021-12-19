956b759a49e0f15ad2d93587a31d533f364ea0a9
kennethluczko <aphromatic@gmail.com>
2021-12-19 01:05:37 -0500

    Theater of Blood and Chambers of Xeric command reactions

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import os
+import random
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+# ----- THEATER OF BLOOD
+@bot.command(name='tob', help='Returns the loot from Theater of Blood.')
+async def tob(tob):
+    tob_loot = [
+        'Scythe of Vitur',
+        'Sanguinesti Staff',
+        "Justicar Helmet"
+    ]
+    response = random.choice(tob_loot)
+    await tob.send(response)
 
-client.run(TOKEN)
+# ----- CHAMBERS OF XERIC
+@bot.command(name='cox', help='Returns the loot from Chambers of Xeric.')
+async def cox(cox):
+    cox_loot = [
+        'Twisted Bow',
+        'Kodai Insignia',
+        "Dexterous Prayer Scroll"
+    ]
+
+    response = random.choice(cox_loot)
+    await cox.send(response)
+
+bot.run(TOKEN)
